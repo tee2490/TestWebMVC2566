b4fb9b4bb2eef3d88cccaa373c94749fac9e0089
tee2490@hotmail.com <tee2490@hotmail.com>
2023-11-25 11:39:59 +0700

    Add project files.

---
+++ WebMVC/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var product = new List<Product>()
+            {
+               new Product() {Id=1,Name="Coffee1",Price=100,Amount=20},
+               new Product() {Id=2,Name="Coffee2",Price=200,Amount=20},
+            };
+
+            return View(product);
+        }
+    }
+}
---
+++ WebMVC/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace WebMVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+    }
+}
