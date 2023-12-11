4557bc08bfbc47586d5e28354582ec861f91d37b
tee2490@hotmail.com <tee2490@hotmail.com>
2023-12-11 09:31:30 +0700

    Code First Database

---
+++ P03_CodeFirst/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_CodeFirst.Models;
+
+namespace P03_CodeFirst.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS; Database=TestProduct67; Trusted_Connection=True; TrustServerCertificate=True");
+            //optionsBuilder.UseSqlite("Data Source=TestProduct67.db");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
---
+++ P03_CodeFirst/Migrations/20231205082904_TestSQLiteDB.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace P03_CodeFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestSQLiteDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    Amount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
